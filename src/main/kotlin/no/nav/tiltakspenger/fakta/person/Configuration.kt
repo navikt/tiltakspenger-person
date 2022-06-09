7366ab024fa9c4b6da7d9342b105ac96668f26d4
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-06-09 17:35:32 +0200

    Make it deploy, maybe 2

--- src/main/kotlin/no/nav/tiltakspenger/fakta/person/Configuration.kt
+++ src/main/kotlin/no/nav/tiltakspenger/fakta/person/Configuration.kt
@@ -4,14 +4,14 @@ internal object Configuration {
 
     private val defaultProperties =
         mapOf(
-            "RAPID_APP_NAME" to "tiltakspenger-vedtak",
+            "RAPID_APP_NAME" to "tiltakspenger-fakta-person",
             "KAFKA_BROKERS" to System.getenv("KAFKA_BROKERS"),
             "KAFKA_CREDSTORE_PASSWORD" to System.getenv("KAFKA_CREDSTORE_PASSWORD"),
             "KAFKA_TRUSTSTORE_PATH" to System.getenv("KAFKA_TRUSTSTORE_PATH"),
             "KAFKA_KEYSTORE_PATH" to System.getenv("KAFKA_KEYSTORE_PATH"),
             "KAFKA_RAPID_TOPIC" to "tpts.rapid.v1",
             "KAFKA_RESET_POLICY" to "latest",
-            "KAFKA_CONSUMER_GROUP_ID" to "tiltakspenger-vedtak-v1"
+            "KAFKA_CONSUMER_GROUP_ID" to "tiltakspenger-fakta-person-v1"
         )
 
     fun asMap(): Map<String, String> = defaultProperties
