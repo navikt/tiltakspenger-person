000e6ba0713c1018fc29e1c61454397390a584bb
sigurdgroneng <sigurdgroneng@nav.no>
2022-06-17 09:42:15 +0200

    Cleanup test

--- app/src/test/kotlin/PersonServiceTest.kt
+++ app/src/test/kotlin/PersonServiceTest.kt
@@ -26,7 +26,7 @@ import java.time.LocalDateTime
 import java.time.Month
 
 class PersonServiceTest {
-    private fun mockRapid(): Triple<TestRapid, PersonService, PDLClient> {
+    private fun mockRapid(): Pair<TestRapid, PDLClient> {
         val metadata = EndringsMetadata(
             endringer = listOf(
                 Endring(
@@ -88,17 +88,17 @@ class PersonServiceTest {
 
         val rapid = TestRapid()
         val pdlClient = mockk<PDLClient>()
-        val personService = PersonService(
+        PersonService(
             rapidsConnection = rapid,
             pdlClient = pdlClient
         )
         coEvery { pdlClient.hentPerson(any()) } returns person.right()
-        return Triple(rapid, personService, pdlClient)
+        return Pair(rapid, pdlClient)
     }
 
     @Test
     fun `skal svare på person-behov`() {
-        val (rapid, _, pdlClient) = mockRapid()
+        val (rapid, pdlClient) = mockRapid()
         val ident = "121212132323"
         // language=JSON
         rapid.sendTestMessage(
@@ -136,7 +136,7 @@ class PersonServiceTest {
 
     @Test
     fun `skal ikke svare på person-behov som er løst`() {
-        val (rapid, _, pdlClient) = mockRapid()
+        val (rapid, pdlClient) = mockRapid()
         val ident = "121212132323"
         // language=JSON
         rapid.sendTestMessage(
@@ -153,16 +153,3 @@ class PersonServiceTest {
         coVerify { pdlClient.hentPerson(any()) wasNot Called }
     }
 }
-
-fun Int.januar(year: Int): LocalDate = LocalDate.of(year, Month.JANUARY, this)
-fun Int.februar(year: Int): LocalDate = LocalDate.of(year, Month.FEBRUARY, this)
-fun Int.mars(year: Int): LocalDate = LocalDate.of(year, Month.MARCH, this)
-fun Int.april(year: Int): LocalDate = LocalDate.of(year, Month.APRIL, this)
-fun Int.mai(year: Int): LocalDate = LocalDate.of(year, Month.MAY, this)
-fun Int.juni(year: Int): LocalDate = LocalDate.of(year, Month.JUNE, this)
-fun Int.juli(year: Int): LocalDate = LocalDate.of(year, Month.JULY, this)
-fun Int.august(year: Int): LocalDate = LocalDate.of(year, Month.AUGUST, this)
-fun Int.september(year: Int): LocalDate = LocalDate.of(year, Month.SEPTEMBER, this)
-fun Int.oktober(year: Int): LocalDate = LocalDate.of(year, Month.OCTOBER, this)
-fun Int.november(year: Int): LocalDate = LocalDate.of(year, Month.NOVEMBER, this)
-fun Int.desember(year: Int): LocalDate = LocalDate.of(year, Month.DECEMBER, this)
