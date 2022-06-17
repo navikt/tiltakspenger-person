e1876296c0f0b94dcf51552abc4113ef9faaaa60
sigurdgroneng <sigurdgroneng@nav.no>
2022-06-17 09:35:09 +0200

    Add missing file
    Co-authored-by: richardmartinsen <richard.martinsen@nav.no>

---
+++ app/src/main/kotlin/no/nav/tiltakspenger/fakta/person/pdl/models/ForelderBarnRelasjon.kt
@@ -0,0 +1,47 @@
+package no.nav.tiltakspenger.fakta.person.pdl.models
+
+import kotlinx.serialization.Serializable
+import no.nav.tiltakspenger.fakta.person.pdl.EndringsMetadata
+import no.nav.tiltakspenger.fakta.person.seralizers.LocalDateSerializer
+import java.time.LocalDate
+
+@Serializable
+enum class ForelderBarnRelasjonRolle {
+    BARN,
+    MOR,
+    FAR,
+    MEDMOR
+}
+
+@Serializable
+data class Personnavn(
+    val fornavn: String,
+    val mellomnavn: String?,
+    val etternavn: String,
+)
+
+@Serializable
+enum class KjoennType {
+    MANN,
+    KVINNE,
+    UKJENT
+}
+
+@Serializable
+data class RelatertBiPerson(
+    val navn: Personnavn?,
+    @Serializable(with = LocalDateSerializer::class)
+    val foedselsdato: LocalDate?,
+    val statsborgerskap: String?,
+    val kjoenn: KjoennType?,
+)
+
+@Serializable
+data class ForelderBarnRelasjon(
+    val relatertPersonsIdent: String,
+    val relatertPersonsRolleval: ForelderBarnRelasjonRolle,
+    val minRolleForPerson: ForelderBarnRelasjonRolle?,
+    val relatertPersonUtenFolkeregisteridentifikator: RelatertBiPerson?,
+    val folkeregistermetadata: FolkeregisterMetadata?,
+    val metadata: EndringsMetadata
+)
