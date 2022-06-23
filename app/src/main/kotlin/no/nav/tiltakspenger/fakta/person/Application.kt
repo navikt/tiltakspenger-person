f2b4a1c5d7bc9bd94271375895cd362d1d43a301
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-06-23 19:39:29 +0200

    Add a DefaultUncaughtExceptionHandler

--- app/src/main/kotlin/no/nav/tiltakspenger/fakta/person/Application.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/fakta/person/Application.kt
@@ -1,7 +1,12 @@
 package no.nav.tiltakspenger.fakta.person
+
+import mu.KotlinLogging
 import no.nav.helse.rapids_rivers.RapidApplication
 
+private val LOG = KotlinLogging.logger {}
 fun main() {
+    Thread.setDefaultUncaughtExceptionHandler { _, e -> LOG.error(e) { e.message } }
+    LOG.info { "Starting tiltakspenger-fakta-person" }
     RapidApplication.create(Configuration.asMap())
         .also {
             PersonService(it)
