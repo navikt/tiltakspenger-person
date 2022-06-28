75c170df84f075faa4d17677d1e54b025c1d0435
sigurdgroneng <sigurdgroneng@nav.no>
2022-06-28 09:10:14 +0200

    Add token expiry-leeway

--- azureAuth/src/main/kotlin/no.nav.tiltakspenger.azureAuth/TokenCache.kt
+++ azureAuth/src/main/kotlin/no.nav.tiltakspenger.azureAuth/TokenCache.kt
@@ -2,12 +2,14 @@ package no.nav.tiltakspenger.azureAuth
 
 import java.time.LocalDateTime
 
+const val LEEWAY_SECONDS: Long = 60
 class TokenCache {
     var token: String? = null
         private set
     private var expires: LocalDateTime? = null
 
-    fun isExpired(): Boolean = expires?.isBefore(LocalDateTime.now()) ?: true
+    fun isExpired(): Boolean = expires
+        ?.isBefore(LocalDateTime.now().plusSeconds(LEEWAY_SECONDS)) ?: true
 
     fun update(accessToken: String, expiresIn: Long) {
         token = accessToken
