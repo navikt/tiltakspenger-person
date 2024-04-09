7a174edad62017215005fd7d4ee245caf039f393
Sneha Desai <snehadesai@Sneha-sin-MacBook-Pro.local>
2024-04-09 11:38:23 +0200

    F Oppgradert versjonen til felles bibliotek tiltakspenger-lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val ktorVersion = "2.3.7"
 val kotestVersion = "5.8.0"
 val jacksonVersion = "2.16.0"
 val mockkVersion = "1.13.8"
-val felleslibVersion = "0.0.85"
+val felleslibVersion = "0.0.93"
 
 plugins {
     application
